ea17e21eae26b329dcee28f52d2cf60b582fe093
Baldo <baldo@lilik.it>
2012-05-06 12:53:00 +0200

    first commit

--- ida_controller.sh
+++ ida_controller.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-ida_info="`ida_controller -d0 -f /dev/ida0`"
-maila="luca.baldesi@stud.unifi.it"
+ida_info="`/usr/sbin/ida_controller -d0 -f /dev/ida0`"
+maila="admin@somehost.com" # TO CHANGE
 
+echo $ida_info
 if [ `echo $ida_info | grep -c "OK"` -lt 1 ]; then
-	echo "Fault found, sending email...."
+	echo "Fault found, sending email... " `date` " " $ida_info >> /var/spool/cron/ida_report
 	echo  "Gandalf raid fault detected!!" $ida_info | /usr/local/sbin/sendmail $maila
 fi
+
